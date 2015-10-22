2b4ea173c87c742ac29cf051de995a1ddc503b7d
Bruno Muniz Azevedo Filho <bruno@mesainc.com.br>
2015-10-22 13:58:50 -0300

    [patient flow almost finished]

--- Doctor/PatientSelectedTableViewController.m
+++ Doctor/PatientSelectedTableViewController.m
@@ -16,6 +16,8 @@
 @property (weak, nonatomic) IBOutlet UILabel* patientSelectedGenderLabel;
 @property (weak, nonatomic) IBOutlet UILabel* patientSelectedAddressLabel;
 
+@property (weak, nonatomic) IBOutlet UIView* patientGrayNameInitialsView;
+
 @end
 
 @implementation PatientSelectedTableViewController
@@ -25,6 +27,10 @@
     self.tableView.tableFooterView = [UIView new];
     self.navigationController.navigationBar.topItem.title = @"";
     [self setupDataFromPatient];
+    
+    self.patientGrayNameInitialsView.layer.cornerRadius = self.patientGrayNameInitialsView.frame.size.height/2;
+    self.patientGrayNameInitialsView.layer.masksToBounds = YES;
+    self.patientGrayNameInitialsView.layer.borderWidth = 0;
 }
 
 
@@ -33,6 +39,47 @@
     self.patientSelectedAgeLabel.text = self.patient.patientAgeString;
     self.patientSelectedGenderLabel.text = self.patient.patientGenderString;
     self.patientSelectedAddressLabel.text = self.patient.patientAdressString;    
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    switch (indexPath.row) {
+        case 1:
+            [self performSegueWithIdentifier:@"clickedInDataSegueId" sender:self];
+            break;
+        case 2:
+            [self performSegueWithIdentifier:@"clickedInAppointmentsSegueId" sender:self];
+            break;
+        case 3:
+            [self performSegueWithIdentifier:@"clickedInExamsSegueId" sender:self];
+            break;
+        case 4:
+                [self performSegueWithIdentifier:@"clickedInTreatmentsSegueId" sender:self];
+            break;
+        case 5:
+                [self performSegueWithIdentifier:@"clickedInEditSegueId" sender:self];
+            break;
+        default:
+            break;
+    }
+
+}
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    if ([segue.identifier isEqualToString:@"clickedInDataSegueId"]) {
+        
+    }
+    else if ([segue.identifier isEqualToString:@"clickedInAppointmentsSegueId"]){
+        
+    }
+    else if ([segue.identifier isEqualToString:@"clickedInExamsSegueId"]){
+        
+    }
+    else if ([segue.identifier isEqualToString:@"clickedInTreatmentsSegueId"]){
+        
+    }
+    else if ([segue.identifier isEqualToString:@"clickedInEditSegueId"]){
+        
+    }
 }
 
 @end
